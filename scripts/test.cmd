213e553b0cc00bd14c6fdb4289ec0942295fc003
Trygve Wastvedt <trygve.wastvedt@autodesk.com>
2025-03-20 14:47:12 -0500

    Set PYTHONNET_PYDLL

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -1,4 +1,5 @@
 set pytest=.\.venv\Scripts\python -m pytest
+for /f "delims=" %%a in ('.\.venv\Scripts\python -m find_libpython') do set PYTHONNET_PYDLL=%%a
 
 dotnet test --runtime any-x64 --logger "console;verbosity=detailed" src/embed_tests/
 %pytest% --runtime coreclr
