d176161248ff57f42d2e8be299d72ca7964a219e
Aparajit Pratap <aparajit.pratap@autodesk.com>
2026-01-13 11:30:05 -0500

    update test

--- src/embed_tests/TestConverter.cs
+++ src/embed_tests/TestConverter.cs
@@ -191,7 +191,7 @@ namespace Python.EmbeddingTest
             int i = 42;
             var pyObject = i.ToPythonAs<IConvertible>();
             var type = pyObject.GetPythonType();
-            Assert.AreEqual(typeof(int).Name, type.Name);
+            Assert.AreEqual("int", type.Name);
         }
 
         // regression for https://github.com/pythonnet/pythonnet/issues/451
